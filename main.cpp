7662ea9e5d4a1fe16e277226da81dc878ff67155
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-03-27 20:06:37 -0300

    setprecision (10)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 
 int main(){
 
@@ -6,14 +7,15 @@ int main(){
     //int número inteiro.
     float b = 9.6;
     //float número de ponto flutuante ocupa 4bytes.
-    double c = 4.6;
+    double c = 4.623145768;
     //double ocupa 8bytes.
 
 
     std::cout<<a<<std::endl;
     std::cout<<b<<std::endl;
     std::cout<<c<<std::endl;
+    std::cout<<std::fixed<<std::setprecision(10)<<c<<std::endl;
 
     return 0;
-    
+
 }
