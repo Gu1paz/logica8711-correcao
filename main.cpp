168a22ddd639e5350e5bdf1a31b9e4089c1d178f
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-03-27 19:41:59 -0300

    first double

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string saudacao = "Boa noite, Guilherme ";
+    int a = 8;
+    //int número inteiro.
+    float b = 9.6;
+    //float número de ponto flutuante ocupa 4bytes.
+    double c = 4.6;
+    //double ocupa 8bytes.
 
-    std::cout<<saudacao<<std::endl;
+
+    std::cout<<a<<std::endl;
+    std::cout<<b<<std::endl;
+    std::cout<<c<<std::endl;
 
     return 0;
+    
 }
