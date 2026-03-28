e35d8688c8274ee1e74105c501094672b42835f2
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-03-27 21:58:30 -0300

    biografia 2

--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@
 int main(){
 
     std::string nome;
-    
+    int idade;
+    float altura;
+
     std::cout<<"Qual seu nome: "<<std::endl;
     std::cin>>nome;
 
+    std::cout<<"qual sua idade "<<std::endl;
+    std::cin>>idade;
+
+    std::cout<<"qual sua altura "<<std::endl;
+    std::cin>>altura;
+    
     std::cout<<"Bem-vindo(a) "<<nome<<"!!"<<std::endl;
+    std::cout<<"qual sua idade? "<<idade<<std::endl;
+    std::cout<<"qual sua altura? "<<altura<<std::endl;
 
     return 0;
 }
