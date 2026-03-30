9806e9c74498a626f61adc50dafc2263762f1a39
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-03-30 20:28:14 -0300

    comparação de variaveis

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include<iostream>
-#include<string>
 
 int main(){
 
-    std::string diaDaSemana;
+    int a;
+    int b;
+    //int a, b;
 
-    std::cout<<"Que dia é hoje: "<<std::endl;
-    std::cin>>diaDaSemana;
-    
-    std::cout<<"Hoje é "<<diaDaSemana<<"!!"<<std::endl;
+    std::cout<<"Digite um valor para A: "<<std::endl;
+    std::cin>>a;
 
+    std::cout<<"Digite um valor para B: "<<std::endl;
+    std::cin>>b;
+
+    std::cout<<(a > b)<<std::endl;
+    std::cout<<(a >= b)<<std::endl;
+    std::cout<<(a != b)<<std::endl;
+         
 
     return 0;
 }
