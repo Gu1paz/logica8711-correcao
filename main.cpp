909683382e95ec63e5e71d67c92d4b653129304f
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-01 21:57:17 -0300

    uso de string

--- main.cpp
+++ main.cpp
@@ -2,17 +2,10 @@
 
 int main(){
 
-    int num1, num2, soma;
+    std::string nome = "Guilherme";
+    std::string sobrenome = "Paz";
 
-    std::cout<< "Digite num 1: "<<"\n";
-    std::cin>>num1;
-
-    std::cout<<"Digite num 2: "<< "\n";
-    std::cin>>num2;
-
-    soma = num1 + num2;
-
-    std::cout<<"O resultado da soma é "<<soma<<"\n";
+    std::cout<<nome + " " + sobrenome<<std::endl;
 
     return 0;
 }
