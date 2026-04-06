b8456f1917dec42ed9b9f64486f8cb8a29217cb6
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-06 20:40:22 -0300

    string 3

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
 
 int main(){
 
-    std::string nome;
-    std::string sobrenome = " Paz";
+    int a = 40;
+    int b = 10;
+    std::string c ="40";
+    std::string d ="10";
 
-    std::cout<<"DIgite seu nome: "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<nome.append(sobrenome);
+    std::cout<<a+b<<std::endl;
+    std::cout<<c+d<<std::endl;
 
 
     return 0;
