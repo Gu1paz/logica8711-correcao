42fcc16bd63bfd91523d720773e17b3d7d37a6a8
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-06 20:45:47 -0300

    error

--- main.cpp
+++ main.cpp
@@ -4,12 +4,10 @@
 int main(){
 
     int a = 40;
-    int b = 10;
-    std::string c ="40";
-    std::string d ="10";
+    std::string b = "10";
 
     std::cout<<a+b<<std::endl;
-    std::cout<<c+d<<std::endl;
+    
 
 
     return 0;
