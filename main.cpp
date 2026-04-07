f8368f22bee0c40d20070c1686721158457021a3
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-06 22:01:10 -0300

    imprime primeiro, terceiro e último caractere da string

--- main.cpp
+++ main.cpp
@@ -3,10 +3,16 @@
 
 int main(){
 
-    int guilherme [5] = {10,20,30,40,50};
+    std::string nome = "Guilherme";
 
-    std::cout<<guilherme[1];
-    
+    std::cout<<nome[0]<<std::endl;
+    std::cout<<nome[2]<<std::endl;
+    std::cout<<nome[nome.length() - 1]<<std::endl;
+    nome[0] = 'A';
+    std::cout<<nome<<std::endl;
+    std::cout<<nome.at(0)<<std::endl;
+    nome.at(0) = 'H';
+    std::cout<<nome;
     
     return 0;
 }
