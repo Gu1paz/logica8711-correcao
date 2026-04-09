ebede62c8be7c8d35f9a4fae8868727e6d426fe7
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-08 21:38:23 -0300

    primeiro IF e ELSE

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 
 int main(){
 
-    std::cout<<std::sqrt(64)<<std::endl;
-    std::cout<<std::round(2.6)<<std::endl;
-    std::cout<<std::log(2)<<std::endl;
+    int a = 5;
+    int b = 2;
+
+    if(a > b){
+        std::cout<<"A é maior que B."<<std::endl;
+    }else{
+        std::cout<<"B é maior ou igual a A."<<std::endl;
+    }
     
     
     return 0;
