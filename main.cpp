3967e02ffbbee627619d965721626c135b5dedb8
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-13 19:21:48 -0300

    implementa verificação de maioridade usando if e else

--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
 #include<iostream>
-#include<cmath>
-#include<string>
 
 int main(){
 
-    std::string user;
-    std::string senha;
+    int idade;
 
-    std::cout<<"Digite seu usuário de acesso: "<<std::endl;
-    std::cin>>user;
+    std::cout<<"Digite a sua idade: "<<std::endl;
+    std::cin>>idade;
 
-    std::cout<<"Digite sua senha: "<<std::endl;
-    std::cin>>senha;
-
-    if(senha == "user123456"){
-        std::cout<<"O usuário "<<user<<" está logado com sucesso!!"<<std::endl;
+    if(idade >= 18){
+        std::cout<<"Você é maior de idade!"<<std::endl;
     }else{
-        std::cout<<"Usuário ou senha inválidos!"<<std::endl;
+        std::cout<<"Você é menor de idade!!"<<std::endl;
     }
-    
+
     return 0;
 }
