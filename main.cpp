c076555b7ca1df560cb3315604860ed7f61f13a5
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-13 19:51:51 -0300

    verificacao de idade usando if, else if e else

--- main.cpp
+++ main.cpp
@@ -7,11 +7,23 @@ int main(){
     std::cout<<"Digite a sua idade: "<<std::endl;
     std::cin>>idade;
 
-    if(idade >= 18){
-        std::cout<<"Você é maior de idade!"<<std::endl;
+    if(idade < 0){
+        std::cout<<"idade inávlida"<<std::endl;
+    }else if(idade <= 2){
+        std::cout<<"Você é um bebê!"<<std::endl;
+    }else if( idade <= 8){
+        std::cout<<"Você é uma criança!"<<std::endl;
+    }else if(idade <= 16){
+        std::cout<<"Você é um(a) adolescente"<<std::endl;
+    }else if(idade <= 60){
+        std::cout<<"Você é um adulto(a)!"<<std::endl;
     }else{
-        std::cout<<"Você é menor de idade!!"<<std::endl;
+        std::cout<<"Você é um(a) sênior!"<<std::endl;
     }
+        
+    
+    
+
 
     return 0;
 }
