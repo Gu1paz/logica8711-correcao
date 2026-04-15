8eaa2e8847b84c8c65026ec266f491069838398c
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-15 20:25:02 -0300

    implementa seleção de dia da semana com switch

--- main.cpp
+++ main.cpp
@@ -1,16 +1,42 @@
-#include<iostream>
-#include<string>
-#include<thread> //para lidar com o tempo de execução
-#include<chrono> //para definir unidades de tempo (ms, segundos)
+#include <iostream>
+#include <string>
+#include <thread> //para lidar com o tempo de execução
+#include <chrono> //para definir unidades de tempo (ms, segundos)
 
-int main(){
+int main()
+{
 
-    int tabuada = 7;
+    int opcao;
 
-    for(int i = 0; i <=10; i++){
-        std::cout<<"O resultado de "<<tabuada<<" x "<<i<<" = "<<tabuada * i<<std::endl;
+    std::cout << "Digite um valor para opção: " << std::endl;
+    std::cin >> opcao;
+
+    switch (opcao)
+    {
+    case 1:
+        std::cout << "domingo!" << std::endl;
+        break;
+    case 2:
+        std::cout << "Segunda!" << std::endl;
+        break;
+    case 3:
+        std::cout << "Terça!" << std::endl;
+        break;
+    case 4:
+        std::cout << "Quarta!" << std::endl;
+        break;
+    case 5:
+        std::cout << "Quinta!" << std::endl;
+        break;
+    case 6:
+        std::cout << "Sexta!" << std::endl;
+        break;
+    case 7:
+        std::cout << "Sábado!" << std::endl;
+        break;
+    default:
+        std::cout << "Número inválido!" << std::endl;
     }
-
 
     return 0;
 }
