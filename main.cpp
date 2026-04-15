9a27ca2d7cc61b6a12ddabbf875fef51550b949e
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-15 19:47:03 -0300

    implementa contagem regressiva usando thread e chrono

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<string>
+#include<thread> //para lidar com o tempo de execução
+#include<chrono> //para definir unidades de tempo (ms, segundos)
 
 int main(){
 
     for(int i = 10; i >=0; i--){
         std::cout<<"A bomba irá explodir em..."<<i<<std::endl;
-    }
 
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); //second -> milliseconds
+    }
+    
 
     return 0;
 }
