1b490597c080b0d04c560aefee41214933c3a2bd
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-15 19:15:51 -0300

    laco de repeticao

--- main.cpp
+++ main.cpp
@@ -3,31 +3,8 @@
 
 int main(){
 
-    std::string nome;
-    float altura;
-    int idade;
-
-    std::cout<<"Qual é o seu nome: "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Qual sua altura: "<<std::endl;
-    std::cin>>altura;
-
-    std::cout<<"Qual sua idade: "<<std::endl;
-    std::cin>>idade;
-
-    std::cout<<"Bem vindo(a) "<<nome<<"!!"<<std::endl;
-    std::cout<<"Sua é altura: "<<altura<<std::endl;
-    std::cout<<"Sua é idade: "<<idade<<std::endl;
-
-    if(idade <= 10){
-        std::cout<<"Você é uma criança"<<std::endl;
-    }else if(idade <=18){
-        std::cout<<"Você é um(a) adolescente"<<std::endl;
-    }else if(idade <= 60){
-        std::cout<<"Você é um(a) adulto(a)"<<std::endl;
-    }else{
-        std::cout<<"Seu velho!!!"<<std::endl;
+    for(int i = 1; i <=10; i++){
+        std::cout<<"Este é um loop!!"<<std::endl;
     }
 
 
