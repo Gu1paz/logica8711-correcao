a806dfa33b9595d17519b743e6a96eeadd6bc88b
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-15 21:01:17 -0300

    pratica soma acumulada com for

--- main.cpp
+++ main.cpp
@@ -3,40 +3,15 @@
 #include <thread> //para lidar com o tempo de execução
 #include <chrono> //para definir unidades de tempo (ms, segundos)
 
-int main()
-{
+int main(){
 
-    int opcao;
+    int soma = 0;
 
-    std::cout << "Digite um valor para opção: " << std::endl;
-    std::cin >> opcao;
-
-    switch (opcao)
-    {
-    case 1:
-        std::cout << "domingo!" << std::endl;
-        break;
-    case 2:
-        std::cout << "Segunda!" << std::endl;
-        break;
-    case 3:
-        std::cout << "Terça!" << std::endl;
-        break;
-    case 4:
-        std::cout << "Quarta!" << std::endl;
-        break;
-    case 5:
-        std::cout << "Quinta!" << std::endl;
-        break;
-    case 6:
-        std::cout << "Sexta!" << std::endl;
-        break;
-    case 7:
-        std::cout << "Sábado!" << std::endl;
-        break;
-    default:
-        std::cout << "Número inválido!" << std::endl;
+    for(int i = 1; i <= 5; i++){
+        soma = soma + i;
     }
+    
+    std::cout<<"Soma é: "<<soma<<std::endl;
 
     return 0;
 }
