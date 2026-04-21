2ffc6bbe215b99cf9cf9da5ecca099aca136d3eb
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-20 21:12:58 -0300

    contagem regressiva com while

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 
 int main(){
 
-    std::string letras[2][4] = {
-        {"A", "B", "C", "D"},
-        {"E", "F", "G", "H"}
-    };
-
-    std::cout<<letras[1][0]<<std::endl;
+    int contagem = 10;
+    
+    while(contagem){
+        std::cout<<contagem<<std::endl;
+        contagem--;
+    }
+    
+    std::cout<<"Feliz ano novo!!"<<std::endl;
 
     return 0;
 }
