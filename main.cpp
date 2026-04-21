d40b23e294da43e71fc399ace6b7b4f22fc017d8
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-20 21:52:13 -0300

    valida entrada com do-while

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 
 int main(){
 
-    int contagem = 10;
-    
-    while(contagem){
-        std::cout<<contagem<<std::endl;
-        contagem--;
+    int num;
+
+    do{
+        std::cout<<"Digite um valor para num: "<<std::endl;
+        std::cin>>num;
+
+        if(num < 1 || num > 10){
+            std::cout<<"Entrada Inválida!!"<<std::endl;
+        }
+    }while(num < 1 || num > 10);{
+        std::cout<<"Obrigado!! Você escolheu o número: "<<num<<std::endl;
     }
-    
-    std::cout<<"Feliz ano novo!!"<<std::endl;
 
     return 0;
 }
