a2e59abf9e6601bc45a8d91186826db2913e4044
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-28 21:08:16 -0300

    implementa função de apresentação formatada

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 #include<string>
 
-void minhaFuncao(std::string nome){
-    std::cout<<nome<<std::endl;
+void minhaFuncao(std::string nome, int idade){
+    std::cout<<"Meu nome é "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
 }
 
 int main(){
 
-    minhaFuncao("Guilherme");
-    minhaFuncao("Nome");
+    minhaFuncao("Guilherme", 22);
 
+    
     return 0;
 }
