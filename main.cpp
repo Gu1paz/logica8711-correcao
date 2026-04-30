65be61f7a7d0f850ace1ad6ec30a03e562a00ace
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-04-30 20:49:14 -0300

    verificar se número é par ou ímpar usando if e else

--- main.cpp
+++ main.cpp
@@ -2,28 +2,21 @@
 
 int main(){
 
-    //brlAmount: valor em rais | exchangeRate: Taxa de Câmbio | usdAmount: Valor em dólares
-    double brlAmount, exchangeRate, usdAmount;
+    int num;
 
-    std::cout<<"----- Currency Converter: BRL to USD -----"<<std::endl;
+    std::cout<<"Por favor digite um número: "<<std::endl;
+    std::cin>>num;
 
-    //Entrada da taxa de câmbio (quanto vale 1 dolar hoje)
-    std::cout<<"Enter the current exchange rate (How much is 1 dollar in Reais?): "<<std::endl;
-    std::cin>>exchangeRate;
+    std::cout<<num%2<<std::endl;
 
-    //Entrada de quantia em Reais que a pessoa deseja converter
-    std::cout<<"Enter the amount in BRL(R$): "<<std::endl;
-    std::cin>>brlAmount;
-
-    //Verificação Lógica: garante que o valor informado seja positivo
-    if(brlAmount > 0){
-        //Calculo: divide o montante em reais pela taxa de câmbio
-        usdAmount = brlAmount / exchangeRate;
-        std::cout<<"You will have $"<<usdAmount<<" US Dollars."<<std::endl;
+    if(num%2 == 0){
+        //todo número par sempre vai sobrar 0
+        std::cout<<"Este número é par: "<<std::endl;
     }else{
-        //Mensagem de erro caso o valor seja zero ou negativo
-        std::cout<<"Error: Please enter a valid amount greater than zero."<<std::endl;
+        //todo número ímpar sempre vai sobrar 1
+        std::cout<<"Este número é ímpar: "<<std::endl;
     }
+
 
 
     return 0;
