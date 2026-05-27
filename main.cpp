27a40d2bba3f54789b905984ce028ef51b74b93d
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-05-26 21:31:56 -0300

    adiciona função para encontrar o maior número em um array

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include<iostream>
 #include<Windows.h>
 
-int cubo(int c){
-    return c * c* c;
+int encontrarMaior(int arr[], int tamanho){
+    int maior = arr[0];
+
+    for(int i = 1; i < tamanho; i++){
+        if(arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+
+    return maior;
 }
 
-int quadrado(int n){
-    return n * n;
-}
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::cout<<"Quadrado de 5: "<<quadrado(5)<<"cubo de 3: "<<cubo(3)<<std::endl;
-    std::cout<<"Quadrado de 8: "<<quadrado(8)<<"cubo de 6: "<<cubo(6)<<std::endl;
-    std::cout<<"Quadrado de 10: "<<quadrado(10)<<"cubo de 9: "<<cubo(9)<<std::endl;
+    int numeros[5];
 
+    std::cout<<"Digite 5 números: "<<std::endl;
+        for(int i = 0; i < 5; i++){
+            std::cout<<"Digite o número "<<(i + 1)<<": ";
+            std::cin>>numeros[i];
+        }
+    std::cout<<"Maior número: "<<encontrarMaior(numeros, 5)<<std::endl;
     return 0;
 }
