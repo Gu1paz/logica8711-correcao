dd47856fefa234077d33a83b88626ffba3fbdf3f
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-05-28 19:40:53 -0300

    fibonacci primeiro exemplo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<tuple>
-#include<string>
 #include<Windows.h>
 
 int main(){
@@ -8,26 +6,18 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string nome;
-    int idade;
-    double altura;
+    int fibonacci[10];
 
-    std::cout<<"Digite seu nome: "<<std::endl;
-    std::getline(std::cin, nome);
+    fibonacci[0] = 1;
+    fibonacci[1] = 1;
 
-    std::cout<<"Digite a sua idade: "<<std::endl;
-    std::cin>>idade;
+    for(int i = 2; i < 10; i++){
+        fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
 
-    std::cout<<"Digite a sua altura: "<<std::endl;
-    std::cin>>altura;
-
-    std::tuple<std::string, int, double> pessoa = std::make_tuple(nome, idade, altura);
-    
-    std::cout<<"\n ----- Dados Salvos na Tupla -----"<<std::endl;
-
-    std::cout<<"Nome: "<<std::get<0>(pessoa)<<std::endl;
-    std::cout<<"Idade: "<<std::get<1>(pessoa)<<" anos."<<std::endl;
-    std::cout<<"Altura: "<<std::get<2>(pessoa)<<"cm"<<std::endl;
+    for(int i = 0; i < 10; i++){
+        std::cout<<fibonacci[i]<<"\n";
+    }
 
     return 0;
 }
