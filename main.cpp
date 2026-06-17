ebb602878054fcd37aae3aeca98989b3024896c2
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-06-17 20:59:51 -0300

    adiciona listagem de campeões da copa usando struct e loop

--- main.cpp
+++ main.cpp
@@ -2,44 +2,30 @@
 #include<string>
 #include<Windows.h>
 
-
+struct CampeonatosCopa{
+    int ano;
+    std::string campeao;
+};
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    std::string time1, time2;
-    int gols1, gols2;
+    CampeonatosCopa copa[5] = {
+        {2022, "Argentina"},
+        {2018, "França"},
+        {2014, "Alemanha"},
+        {2010, "Espanha"},
+        {2006, "Italia"}
+    };
 
-    std::cout<<"==== COMPARADOR DE PLACAR ===="<<std::endl;
+    std::cout<<"==== Campeões da Copa ===="<<std::endl;
     std::cout<<std::endl;
 
-    std::cout<<"Time 1: ";
-    std::cin>>time1;
-    std::cout<<"Gols time 1: ";
-    std::cin>>gols1;
-
-    std::cout<<"Time 2: ";
-    std::cin>>time2;
-    std::cout<<"Gols time 2: ";
-    std::cin>>gols2;
-
-    std::cout<<std::endl;
-    std::cout<<"==== RESULTADO ===="<<std::endl;
-    std::cout<<time1<<" "<<gols1<<" x "<<gols2<<" "<<time2<<std::endl;
-    std::cout<<std::endl;
-
-    if(gols1 > gols2){
-        std::cout<<"Vencedor!! "<<time1<<std::endl;
-    }else if(gols2 > gols1){
-        std::cout<<"Vencedor!! "<<time2<<std::endl;
-    }else{
-        std::cout<<"Empate!!"<<std::endl;
+    for(int i = 0; i < 5; i++){
+        std::cout<<copa[i].ano<<" - Campeão: "<<copa[i].campeao<<std::endl;
     }
 
-    std::cout<<std::endl;
-
-    
     return 0;
 }
