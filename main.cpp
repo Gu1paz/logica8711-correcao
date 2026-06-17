669a1ad49c7efb7954453c7cc0c708a07198d49c
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-06-17 19:02:31 -0300

    jogo da forca

--- main.cpp
+++ main.cpp
@@ -1,22 +1,70 @@
 #include<iostream>
-#include<string>
+#include<cctype>
 #include<Windows.h>
-
-int horner(int * coeficientes, int grau, int x){
-    int resultado = coeficientes[grau];
-    for(int i = grau - 1; i >= 0; i--){
-        resultado = resultado * x + coeficientes[i];
-    }
-    return resultado;
-}
 
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    int coef[] = {5, 4, 3, 2};
-    std::cout<<"p(2) = "<<horner<<(coef, 3, 2)<<std::endl;
+    std::string palavra = "programação";
+    std::string advinhada = "";
+    int erros = 0;
+    int maxErros = 6;
+    bool ganhou = false;
+
+    for(int i = 0; i < palavra.length(); i++){
+        advinhada += "_";
+    }
+    std::cout<<"==== JOGO DA FORCA ===="<<std::endl;
+    std::cout<<"Advinhe a palavra!"<<std::endl;
+    std::cout<<std::endl;
+
+    while(erros < maxErros && !ganhou){
+        std::cout<<"Palavra: ";
+        for(int i = 0; i < advinhada.length(); i++){
+            std::cout<<advinhada[i]<<" ";
+        }
+        std::cout<<std::endl;
+        std::cout<<"Erros: "<<erros<<"/"<<maxErros<<std::endl;
+        std::cout<<std::endl;
+
+        char letra;
+        std::cout<<"Digite uma letra: ";
+        std::cin>>letra;
+        letra = std::tolower(letra);
+
+        std::cout<<std::endl;
+
+        bool encontrou = false;
+
+        for(int i = 0; i < palavra.length(); i++){
+            if(palavra[i] == letra){
+                advinhada[i] = letra;
+                encontrou = true;
+            }
+        }
+        if(!encontrou){
+            std::cout<<"Letra errada!"<<std::endl;
+            erros++;
+        }else{
+            std::cout<<"Letra encontrada!"<<std::endl;
+        }
+        std::cout<<std::endl;
+
+        if(advinhada == palavra){
+            ganhou = true;
+        } 
+    }
+    std::cout<<"========="<<std::endl;
+    if(ganhou){
+        std::cout<<"Parabéns! Você venceu!"<<std::endl;
+        std::cout<<"A palavra era: "<<palavra<<std::endl;
+    }else{
+        std::cout<<"Game over! Você perdeu!"<<std::endl;
+        std::cout<<"A palavra era: "<<palavra<<std::endl;
+    }
+    
 
 
     return 0;
