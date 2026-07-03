ef29b970e9dca94152659122416479d0f335aeb8
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-07-02 21:56:41 -0300

    vector maior e menor

--- main.cpp
+++ main.cpp
@@ -10,22 +10,34 @@ int main(){
 
     std::vector<int>numeros;
 
-    std::cout<<"==== Acessar elemento ===="<<std::endl;
+    std::cout<<"==== Maior e menor ===="<<std::endl;
     std::cout<<std::endl;
 
-    numeros.push_back(100);
-    numeros.push_back(200);
-    numeros.push_back(300);
-    numeros.push_back(400);
+    numeros.push_back(15);
+    numeros.push_back(8);
+    numeros.push_back(25);
+    numeros.push_back(3);
+    numeros.push_back(42);
 
     std::cout<<"Vector: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
     }
     std::cout<<std::endl;
+    int maior = numeros[0];
+    int menor = numeros[0];
 
-    std::cout<<"Elemento na posição 0: "<<numeros[0]<<std::endl;
-    std::cout<<"Elemento na posição 2: "<<numeros[2]<<std::endl;
-    std::cout<<"Último elemento: "<<numeros[numeros.size() - 1]<<std::endl;
+    for(int i = 0; i < numeros.size(); i++){
+        if(numeros[i] > maior){
+            maior = numeros[i];
+        }
+        if(numeros[i] < menor){
+            menor = numeros[i];
+        }
+    }
+    std::cout<<"Maior: "<<maior<<std::endl;
+    std::cout<<"Menor: "<<menor<<std::endl;
+    
+   
     return 0;
 }
