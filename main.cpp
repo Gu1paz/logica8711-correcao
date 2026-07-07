454aca4925814cfb5d016c06d9dc7d890985aeeb
GUILHERME ANTONIO PAZ ANDRADE <05373755036@senacrs.edu.br>
2026-07-07 19:47:02 -0300

    Implementa busca linear de elemento em um vector

--- main.cpp
+++ main.cpp
@@ -9,29 +9,32 @@ int main(){
     SetConsoleCP(CP_UTF8);
 
     std::vector<int> numeros;
-    std::cout<<"Contar ocorrências: "<<std::endl;
 
-    numeros.push_back(5);
-    numeros.push_back(10);
-    numeros.push_back(5);
-    numeros.push_back(15);
-    numeros.push_back(15);
-    numeros.push_back(5);
-    numeros.push_back(15);
+    std::cout<<"Buscar elemento: "<<std::endl;
+
+    numeros.push_back(100);
+    numeros.push_back(200);
+    numeros.push_back(300);
+    numeros.push_back(400);
 
     std::cout<<"Vector: ";
     for(int i = 0; i < numeros.size(); i++){
         std::cout<<numeros[i]<<" ";
     }
 
-    int contador = 0;
+    int buscado = 300;
+    int posicao = -1;
+
     for(int i = 0; i < numeros.size(); i++){
-        if(numeros[i] == 5){
-            contador++;
+        if(numeros[i] == buscado){
+            posicao = i;
+            break;
         }
     }
-
-    std::cout<<"O número 5 aparece: "<<contador<<" vezes!"<<std::endl;
-
+    if(posicao != -1){
+        std::cout<<"Número "<<buscado<<" encontrado na posição "<<posicao<<std::endl;
+    }else{
+        std::cout<<"Número não encontrado!"<<std::endl;
+    }
     return 0;
 }
